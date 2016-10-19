5e5742ea0c3642bb774bf64c5b6cd353dd0e5055
Jarad Niemi <niemi@iastate.edu>
2016-10-19 09:43:38 -0500

    Add meta data

---
+++ data-raw/crop_rotation.R
@@ -0,0 +1,2 @@
+crop_rotation <- readr::read_csv("crop_rotation.csv")
+devtools::use_data(crop_rotation, overwrite = TRUE)
---
+++ data-raw/watersheds.R
@@ -0,0 +1,2 @@
+watersheds <- readr::read_csv("watersheds.csv")
+devtools::use_data(watersheds, overwrite = TRUE)
