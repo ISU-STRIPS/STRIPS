a1d478a6575fd9d269d8e42a906e06d306fd3d89
Jarad Niemi <niemi@iastate.edu>
2016-10-19 09:54:41 -0500

    Add documentation

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Watershed details
+#'
+#' Watershed meta data to be used for analyses.
+#'
+#' @format A data frame with 11 variables: \code{watershed}, \code{NAD83_X},
+#'   \code{NAD83_Y}, \code{site}, \code{size_ha}, \code{slope_pct},
+#'   \code{location_and_percentage_of_grass_filers}, \code{treatment}, and
+#'   \code{prairie_pct}.
+"watersheds"
+
+#' Crop rotation
+#'
+#' The type of crop in the watersheds by year.
+#'
+#' @format A data frame with 2 variables: \code{year} and \code{crop}.
+"crop_rotation"
