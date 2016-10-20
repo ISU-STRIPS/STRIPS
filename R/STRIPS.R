371057089fe18e3b7951cc6f03d0e29016003714
Jarad Niemi <niemi@iastate.edu>
2016-10-20 09:56:06 -0500

    Add package documentation
    also import STRIPSMeta

---
+++ R/STRIPS.R
@@ -0,0 +1,17 @@
+#' STRIPS: A package for loading publically available STRIPS data.
+#'
+#' The STRIPS package provides a user-friendly mechanism to load all publically
+#' availabe STRIPS data by simply installing and then loading this package. As
+#' data are made public, this package will depend on those packages and thus
+#' reinstalling the updated version of this package will allow access to those
+#' data newly made public.
+#'
+#' @section STRIPS data sets:
+#' \code{STRIPSMeta}
+#'
+#' @seealso \code{\link{STRIPSMeta}}
+#'
+#' @import STRIPSMeta
+#' @docType package
+#' @name STRIPS
+NULL
